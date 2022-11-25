8db52a681288151ed2d058b0e8233481f5314039
alevans99 <alevans99@gmail.com>
2022-11-25 17:18:45 +0000

    Adds React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/pages/LandingPage'
 
 function App() {
   return (
-    <div id="App" className="App">
-      <LandingPage/>
-    </div>
+    <BrowserRouter>
+
+      <div id="App" className="App">
+        <Routes>
+          <Route
+            path='/'
+            element={<LandingPage/>}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 export default App
