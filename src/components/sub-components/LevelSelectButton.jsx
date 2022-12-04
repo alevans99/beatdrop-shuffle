aeac024f1d0d824a59889863bdf792dfd83c3868
alevans99 <alevans99@gmail.com>
2022-12-04 13:35:10 +0000

    Updates level select page

---
+++ src/components/sub-components/LevelSelectButton.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import '../../styles/sub-components/LevelSelectButton.css'
+import {PropTypes} from 'prop-types'
+import padlock from '../../assets/padlock.png'
+function LevelSelectButton({buttonText, buttonImage, locked, levelNumber}) {
+  const [buttonHover, setButtonHover] = useState(false)
+
+  const handleButtonHover = (hoverState) =>{
+    setButtonHover(() => {
+      return hoverState
+    })
+  } 
+
+  return (
+    <div className={`level-select-${levelNumber} LevelSelectButton`}  >
+      <div className='button-image-container'>
+        <img className={`button-image ${locked ? 'locked' : ''}` } src={buttonImage} ></img>
+        <div className='button-image-overlay'
+          onMouseEnter={() => {handleButtonHover(true)}}
+          onMouseLeave={() => {handleButtonHover(false)}}
+        > 
+          <img className={`level-select-locked-image ${locked ? '' : 'hidden'}`} src={padlock}></img>
+          <h2 className={`button-text ${buttonHover && !locked ? '' : 'hidden'}`}>{buttonText}</h2>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+LevelSelectButton.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  buttonImage: PropTypes.string.isRequired,
+  locked: PropTypes.bool.isRequired,
+  levelNumber: PropTypes.number.isRequired
+}
+
+export default LevelSelectButton
