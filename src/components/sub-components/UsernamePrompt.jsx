6b5efb20cc263d3df5a9b36dc55752d4816388b4
alevans99 <alevans99@gmail.com>
2022-12-31 12:45:08 +0000

    Adds username form

--- src/components/sub-components/UsernamePrompt.jsx
+++ src/components/sub-components/UsernamePrompt.jsx
@@ -6,6 +6,10 @@ import '../../styles/sub-components/UsernamePrompt.css'
 
 function UsernamePrompt({showUsernamePrompt}) {
 
+  const handleFormSubmission = (e) => {
+    e.preventDefault()
+  }
+
   return (showUsernamePrompt &&
     <div 
       id="UsernamePrompt" 
@@ -15,8 +19,14 @@ function UsernamePrompt({showUsernamePrompt}) {
         size={{height: '80%', width: '80%'}} 
         modalDisplayed={true}>
         <div className='name-prompt-modal-content'>
-          <h1 className='name-prompt-heading'>Hi There! Please enter a username</h1>
-          <h5 className='name-prompt-subtitle'>This will be used to keep track of High-Scores</h5>
+          <div className='name-prompt-header'>
+            <h1 className='name-prompt-heading'>Hi There! Please enter a username</h1>
+            <h5 className='name-prompt-subtitle'>This will be used to keep track of High-Scores</h5>
+          </div>
+          <form className='name-prompt-form' onSubmit={handleFormSubmission}>
+            <input type="text" name="usernameInput" id="username-input" className='username-input' />
+            <button type='button' className='username-input-button'>Submit</button>
+          </form>
         </div>
       </ModalComponent>
 
