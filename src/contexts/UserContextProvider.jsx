4acdd9dfc0395b769d5439f08bd5b66af5e72ce6
alevans99 <alevans99@gmail.com>
2023-01-06 11:28:18 +0000

    Adds score update functionality

--- src/contexts/UserContextProvider.jsx
+++ src/contexts/UserContextProvider.jsx
@@ -23,6 +23,56 @@ export function UserContextProvider({ children }) {
     setUserScores(newScores)
   }
 
+  /**
+   * Checks to see if the user has a new high score
+   * @param {*} newScore - The newly achived score from the game
+   * @param {*} level - The level the user was playing
+   * @returns - An object informing if there is a new high score and its details
+   */
+  function checkNewUserScore(newScore, level) {
+
+    //Get an array of high scores for the level
+    const existingScores = userScores[level] !== undefined ? [...Object.values(userScores[level])] : []
+
+    //Check if the new score is higher than at least one of the previous scores
+    if (existingScores[4] < newScore){
+      let newScoreIndex = 0
+      //Create a new object to store in state
+      const newScoresObject = {}
+      Object.keys(userScores).forEach((key) => {
+        newScoresObject[key] = {...userScores[key]}
+      })
+      //Update the level array with the new top 5 scores
+      for (let i = 0; i < existingScores.length; i++){
+        if (existingScores[i] < newScore){
+          existingScores.splice(i, 0, newScore)
+          newScoreIndex = i
+          break
+        }
+      }
+      //Remove the previous lowest score
+      existingScores.pop()
+
+      existingScores.forEach((score, i) => {
+        newScoresObject[level][i + 1] = score
+      })
+  
+      //Update the scores in state and localstorage
+      updateUserScores(newScoresObject)
+
+      return {
+        newScoreAchived: true,
+        level,
+        newScore,
+        newScorePosition: newScoreIndex + 1
+      }
+    }
+
+    return {
+      newScoreAchived: false
+    }
+  }
+
 
   useEffect(() => {
     //Set the username and scores from local storage
@@ -43,7 +93,7 @@ export function UserContextProvider({ children }) {
   }, [])
   
   return (
-    <UserContext.Provider value={{ username, updateUsername, userScores, updateUserScores }}>
+    <UserContext.Provider value={{ username, updateUsername, userScores, updateUserScores, checkNewUserScore }}>
       {children}
     </UserContext.Provider>
   )
